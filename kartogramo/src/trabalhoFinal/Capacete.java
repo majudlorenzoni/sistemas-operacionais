1547384f7d0c5d571e74c254a9bbfe89da9887ef
majudlorenzoni <juliamaria892@gmail.com>
2024-08-25 02:20:47 -0300

    refactor: retirando classe mãe recursos

--- kartogramo/src/trabalhoFinal/Capacete.java
+++ kartogramo/src/trabalhoFinal/Capacete.java
@@ -1,8 +1,38 @@
 package trabalhoFinal;
 
+public class Capacete {
+    private int id;
+    private Cliente clienteAtual;
+    private boolean disponivel;
 
-public class Capacete extends Recursos {
-  public Capacete(int id) {
-    super(id);
-  }
+    public Capacete(int id) {
+        this.id = id;
+        this.disponivel = true; // Inicialmente disponível
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Cliente getClienteAtual() {
+        return clienteAtual;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public synchronized void alocar(Cliente cliente) {
+        if (disponivel) {
+            this.clienteAtual = cliente;
+            this.disponivel = false;
+        } else {
+            throw new IllegalStateException("Capacete já alocado");
+        }
+    }
+
+    public synchronized void liberar() {
+        this.clienteAtual = null;
+        this.disponivel = true;
+    }
 }
--- kartogramo/src/trabalhoFinal/Kart.java
+++ kartogramo/src/trabalhoFinal/Kart.java
@@ -1,7 +1,38 @@
 package trabalhoFinal;
 
-public class Kart extends Recursos {
-  public Kart(int id) {
-    super(id);
-  }
+public class Kart {
+    private int id;
+    private Cliente clienteAtual;
+    private boolean disponivel;
+
+    public Kart(int id) {
+        this.id = id;
+        this.disponivel = true; // Inicialmente disponível
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Cliente getClienteAtual() {
+        return clienteAtual;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public synchronized void alocar(Cliente cliente) {
+        if (disponivel) {
+            this.clienteAtual = cliente;
+            this.disponivel = false;
+        } else {
+            throw new IllegalStateException("Kart já alocado");
+        }
+    }
+
+    public synchronized void liberar() {
+        this.clienteAtual = null;
+        this.disponivel = true;
+    }
 }
